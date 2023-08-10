7f1df21636d7a0df2bf6f2d9b400499380530797
MaZyAkAkUn <mzxk26@gmail.com>
2023-08-10 14:45:03 +0200

    Finished Lesson 10, Email Support

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from flask_mail import Mail
 import os
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
+mail = Mail(app)
 from app import routes, models, errors
 
 if not app.debug:
