71f5e3a51984b115b168b6d95f6971d7adebaeee
MaZyAkAkUn <mzxk26@gmail.com>
2023-08-11 12:37:24 +0200

    Lesson 11, facelift

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
 import os
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -15,7 +17,9 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 from app import routes, models, errors
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
